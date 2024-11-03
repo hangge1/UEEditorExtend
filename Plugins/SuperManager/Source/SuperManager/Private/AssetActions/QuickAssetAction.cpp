75d27f74c334a4684083457ca7c16654ead74208
hangdada <824362330@qq.com>
2024-11-03 21:54:36 +0800

    Add FMessageDialog & Notify capsulation in debugheader

--- Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickAssetAction.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickAssetAction.cpp
@@ -4,9 +4,41 @@
 #include "AssetActions/QuickAssetAction.h"
 #include "DebugHeader.h"
 
-void UQuickAssetAction::TestFunc()
-{
-	Print(TEXT("Working"), FColor::Cyan);
-	PrintLog(TEXT("Working"));
+#include "EditorUtilityLibrary.h"
+#include "EditorAssetLibrary.h"
 
+#include "Misc/MessageDialog.h"
+
+void UQuickAssetAction::DuplicateAssets(int32 NumOfDuplicates)
+{
+	if (NumOfDuplicates <= 0)
+	{
+		ShowMsgDialog(EAppMsgType::Ok, TEXT("Please enter a VALID number"));
+		return;
+	}
+
+	TArray<FAssetData> SeletedAssetsData = UEditorUtilityLibrary::GetSelectedAssetData();
+	uint32 Counter = 0;
+
+	for (const FAssetData& CurrentAssetData : SeletedAssetsData)
+	{
+		for (int32 i = 0; i < NumOfDuplicates; i++)
+		{
+			const FString SourceAssetPath = CurrentAssetData.ObjectPath.ToString();
+			const FString NewDuplicatedAssetName = CurrentAssetData.AssetName.ToString() + TEXT("_") + FString::FromInt(i + 1);
+			const FString NewPathName = FPaths::Combine(CurrentAssetData.PackagePath.ToString(), NewDuplicatedAssetName);
+
+			if (UEditorAssetLibrary::DuplicateAsset(SourceAssetPath, NewPathName))
+			{
+				UEditorAssetLibrary::SaveAsset(NewPathName, false);
+				++Counter;
+			}
+		}
+	}
+
+	if (Counter > 0)
+	{
+		ShowNotifyInfo(TEXT("Successfully duplicated " + FString::FromInt(Counter) + " files"));
+	}
+	
 }
--- Plugins/SuperManager/Source/SuperManager/Public/DebugHeader.h
+++ Plugins/SuperManager/Source/SuperManager/Public/DebugHeader.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "Misc/MessageDialog.h"
+
+#include "Framework/Notifications/NotificationManager.h"
+#include "Widgets/Notifications/SNotificationList.h"
+
 
 void Print(const FString& Message, const FColor& Color)
 {
@@ -12,4 +17,26 @@ void Print(const FString& Message, const FColor& Color)
 void PrintLog(const FString& Message)
 {
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
+}
+
+EAppReturnType::Type ShowMsgDialog(EAppMsgType::Type MsgType, const FString& Message, bool bShowMsgAsWarning = true)
+{
+	if (bShowMsgAsWarning)
+	{
+		FText MsgTitle = FText::FromString(TEXT("Warning"));
+		return FMessageDialog::Open(EAppMsgType::Ok,FText::FromString(Message),&MsgTitle);
+	}
+	else
+	{
+		return FMessageDialog::Open(EAppMsgType::Ok, FText::FromString(Message));
+	}
+}
+
+void ShowNotifyInfo(const FString& Message)
+{
+	FNotificationInfo NotifyInfo(FText::FromString(Message));
+	NotifyInfo.bUseLargeFont = true;
+	NotifyInfo.FadeOutDuration = 7.f;
+	
+	FSlateNotificationManager::Get().AddNotification(NotifyInfo);
 }
