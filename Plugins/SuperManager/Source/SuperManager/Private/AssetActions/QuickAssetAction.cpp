fc9d67de87da0ff0c719c36c20a89e649ee45ab1
hangdada <824362330@qq.com>
2024-11-03 20:16:11 +0800

    Add Debug Header

--- Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickAssetAction.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickAssetAction.cpp
@@ -2,11 +2,11 @@
 
 
 #include "AssetActions/QuickAssetAction.h"
+#include "DebugHeader.h"
 
 void UQuickAssetAction::TestFunc()
 {
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Cyan, TEXT("Working"));
-	 }
+	Print(TEXT("Working"), FColor::Cyan);
+	PrintLog(TEXT("Working"));
+
 }
