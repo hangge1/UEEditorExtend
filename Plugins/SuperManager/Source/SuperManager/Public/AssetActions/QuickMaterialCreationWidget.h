859f84114208a6fc95babea2c3597bd6230efc9a
hangdada <824362330@qq.com>
2024-11-06 19:02:34 +0800

    Create QuickMaterialCreationWidget C++ Class and blueprint" ;

---
+++ Plugins/SuperManager/Source/SuperManager/Public/AssetActions/QuickMaterialCreationWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "QuickMaterialCreationWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERMANAGER_API UQuickMaterialCreationWidget : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+	
+};
